2f6a530b0fd28fdda3e9aa90cf3bddeae2bb2d1a
erbridge <f@erbridge.co.uk>
2023-03-05 23:57:25 +0000

    Pick a random question if `q` and `a` aren't provided to quiz
    We don't guard for a plant having missing data for now.

--- app/controllers/plant_quiz_controller.rb
+++ app/controllers/plant_quiz_controller.rb
@@ -1,7 +1,19 @@
 class PlantQuizController < ApplicationController
+  QUESTION_COMBINATIONS = [
+    {question_attribute: :common_name, answer_attribute: :botanical_name},
+    {question_attribute: :botanical_name, answer_attribute: :common_name},
+    {question_attribute: :description, answer_attribute: :common_name}
+  ].freeze
+
   def question
-    @question_attribute = (params[:q] || "common_name").to_sym
-    @answer_attribute = (params[:a] || "botanical_name").to_sym
+    if params[:q] && params[:a]
+      @question_attribute = params[:q].to_sym
+      @answer_attribute = params[:a].to_sym
+    else
+      combo = QUESTION_COMBINATIONS.sample
+      @question_attribute = combo[:question_attribute]
+      @answer_attribute = combo[:answer_attribute]
+    end
 
     @plant = Plant.all.sample
     @question_value = @plant[@question_attribute]
