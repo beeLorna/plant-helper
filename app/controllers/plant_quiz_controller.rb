6cf40dd7057c038ec4ed3754fe8f3767157ec154
erbridge <f@erbridge.co.uk>
2023-03-05 23:49:24 +0000

    Support picking any plant attribute for a quiz answer
    For example: "/plant_quiz/question?q=description&a=common_name"

--- app/controllers/plant_quiz_controller.rb
+++ app/controllers/plant_quiz_controller.rb
@@ -1,13 +1,19 @@
 class PlantQuizController < ApplicationController
   def question
-    @plant = Plant.all.sample
     @question_attribute = (params[:q] || "common_name").to_sym
+    @answer_attribute = (params[:a] || "botanical_name").to_sym
+
+    @plant = Plant.all.sample
     @question_value = @plant[@question_attribute]
   end
 
   def answer
-    @plant = Plant.find(params[:plant])
-    @given_answer = params[:botanical_name]
-    @expected_answer = @plant.botanical_name
+    plant_id = params[:plant]
+
+    @answer_attribute = params[:answer_attribute].to_sym
+    @given_answer = params[@answer_attribute]
+
+    @plant = Plant.find(plant_id)
+    @expected_answer = @plant[@answer_attribute]
   end
 end
