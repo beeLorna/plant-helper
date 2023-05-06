5c7b25400655e7119c7043a088b1d12906be583e
Lorna <lorna@dxw.com>
2023-05-06 12:41:07 +0100

    Add week 14 plant identification information

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,5 +1,5 @@
 class Plant < ActiveYaml::Base
   set_root_path "data/plants"
   use_multiple_files
-  set_filenames "1", "2", "3", "4", "5", "6", "11", "12", "13"
+  set_filenames "1", "2", "3", "4", "5", "6", "11", "12", "13", "14"
 end
