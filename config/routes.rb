a9e3a969b81d29e97f91fc37a232888af1207d2a
Lorna <lorna@dxw.com>
2023-06-06 22:04:37 +0100

    Show root index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "plants#index"
-  resources :plants, only: [:show]
+  root "root#home"
+  resources :plants, only: [:index, :show]
 
   scope :plant_quiz, as: :plant_quiz do
     get :question, to: "plant_quiz#question"
